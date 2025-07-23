db7697b64a7792bf4cd9b6892ccf4aad00d56dc4
Raphael <raphael.larsen@proton.me>
2025-07-22 21:40:31 -0400

    Formatting.

--- voxel/src/data/chunk/space/mod.rs
+++ voxel/src/data/chunk/space/mod.rs
@@ -4,16 +4,14 @@
 /// - `VoxelLocalPos` the voxels position in space relative to a chunks origin
 /// - `ChunkPos` the chunks position in space relative to the objects origin
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct VoxelGlobalPos {
-	pub chunk_pos: ChunkPos,
-	pub local_pos: VoxelLocalPos,
+pub struct GlobalVoxelPos {
+    pub chunk_pos: ChunkPos,
+    pub local_pos: LocalVoxelPos,
 }
 
 /// A voxel position relative to a chunks origin
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct VoxelLocalPos(pub u8, pub u8, pub u8);
-
-
+pub struct LocalVoxelPos(pub u8, pub u8, pub u8);
 
 /// The position of a chunk relative to the origin
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
