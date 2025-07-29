0f3dc662d63d70f065268420d8ea4d3c29b4da01
Raphael <raphael.larsen@proton.me>
2025-07-29 10:07:34 -0400

    Changed to U8 in chunk indexing

--- voxel/src/data/chunk.rs
+++ voxel/src/data/chunk.rs
@@ -1,12 +1,13 @@
-use super::voxel::{Voxel, self};
-use bevy::math::U16Vec3;
+use super::voxel::{self, Voxel};
+use bevy::math::U8Vec3;
 use ndshape::{ConstPow2Shape3u16, ConstShape};
 
 const BITS: u8 = 5;
 
 pub const LENGTH_IN_VOXELS: u16 = 1 << BITS;
 
-type ChunkShape = ConstPow2Shape3u16<LENGTH_IN_VOXELS, LENGTH_IN_VOXELS, LENGTH_IN_VOXELS>;
+type ChunkShape =
+    ConstPow2Shape3u16<LENGTH_IN_VOXELS, LENGTH_IN_VOXELS, LENGTH_IN_VOXELS>;
 
 const VOLUME_IN_VOXELS: usize = (LENGTH_IN_VOXELS as usize).pow(3);
 
@@ -33,15 +34,15 @@ impl Chunk {
                     *self = Filled(base);
                 }
                 can_collapse
-            },
+            }
         }
     }
 }
 
-pub fn pos_to_index(pos: U16Vec3) -> usize {
-    ChunkShape::linearize(pos.into()) as usize
+pub fn pos_to_index(pos: U8Vec3) -> usize {
+    ChunkShape::linearize(pos.as_u16vec3().into()) as usize
 }
 
-pub fn index_to_pos(index: usize) -> U16Vec3 {
-    ChunkShape::delinearize(index as u16).into()
+pub fn index_to_pos(index: usize) -> U8Vec3 {
+    ChunkShape::delinearize(index as u16).map(|d| d as u8).into()
 }
