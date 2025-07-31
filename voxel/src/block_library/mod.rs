a73cb26c48ec16dd6cd455aad7e449bcb79eff94
Raphael <raphael.larsen@proton.me>
2025-07-31 09:58:16 -0400

    Formatting

--- voxel/src/block_library/mod.rs
+++ voxel/src/block_library/mod.rs
@@ -1,9 +1,16 @@
 use anyhow::Context;
 use bevy::{
-    asset::{io::Reader, Asset, AssetLoader, LoadContext}, math::bounding::Aabb3d, prelude::*, reflect::TypePath, tasks::ConditionalSendFuture
+    asset::{Asset, AssetLoader, LoadContext, io::Reader},
+    math::bounding::Aabb3d,
+    prelude::*,
+    reflect::TypePath,
+    tasks::ConditionalSendFuture,
 };
-use bevy_materialize::{prelude::JsonMaterialDeserializer, MaterializePlugin};
-use std::collections::HashMap;
+use bevy_materialize::{MaterializePlugin, prelude::JsonMaterialDeserializer};
+use std::{
+    collections::HashMap,
+    path::{Path, PathBuf},
+};
 
 /// Deserializable structs. The only difference is that they store sub-assets as paths instead of handles
 mod raw;
@@ -18,23 +25,23 @@ pub struct BlockLibrary {
 
 #[derive(Debug)]
 pub struct Material {
-	pub handle: Handle<StandardMaterial>,
-	pub size: UVec2,
+    pub handle: Handle<StandardMaterial>,
+    pub size: UVec2,
 }
 
 #[derive(Debug)]
 pub struct BlockVariant {
-	pub display_name: String,
-	pub collision_aabbs: Vec<Aabb3d>,
-	pub is_transparent: bool,
-	pub block_model: BlockModel,
+    pub display_name: String,
+    pub collision_aabbs: Vec<Aabb3d>,
+    pub is_transparent: bool,
+    pub block_model: BlockModel,
 }
 
 #[derive(Debug)]
 pub enum BlockModel {
-	Empty,
-	Cube(BlockModelCube),
-	Mesh(BlockModelMesh),
+    Empty,
+    Cube(BlockModelCube),
+    Mesh(BlockModelMesh),
 }
 
 // the raw::BlockModelCube has no sub-assets and so can be used as is
@@ -42,8 +49,8 @@ pub use raw::BlockModelCube;
 
 #[derive(Debug)]
 pub struct BlockModelMesh {
-	pub handle: Handle<Mesh>,
-	pub material_index: usize,
+    pub handle: Handle<Mesh>,
+    pub material_index: usize,
 }
 
 #[derive(Default)]
@@ -66,7 +73,8 @@ impl AssetLoader for BlockLibraryLoader {
     ) -> impl ConditionalSendFuture<Output = Result<Self::Asset, Self::Error>> {
         async move {
             let mut bytes = Vec::new();
-            reader.read_to_end(&mut bytes)
+            reader
+                .read_to_end(&mut bytes)
                 .await
                 .context("Failed to read asset bytes for BlockLibrary")?;
 
@@ -74,21 +82,18 @@ impl AssetLoader for BlockLibraryLoader {
                 materials: raw_materials,
                 blocks: raw_blocks,
             } = serde_json::de::from_slice::<raw::BlockLibrary>(&bytes)
-				.context("Failed to deserialize raw::BlockLibrary from JSON")?;
+                .context("Failed to deserialize raw::BlockLibrary from JSON")?;
 
-            let materials = raw_materials.into_iter().map(|m| {
-				let raw::Material {
-					path,
-					size,
-				} = m;
+            let materials = raw_materials
+                .into_iter()
+                .map(|m| {
+                    let raw::Material { path, size } = m;
 
-				let handle = load_context.load(path);
-				
-				Material {
-					handle,
-					size,
-				}
-			}).collect::<Vec<_>>();
+                    let handle = load_context.load(resolve_path(load_context, &path));
+
+                    Material { handle, size }
+                })
+                .collect::<Vec<_>>();
 
             let capacity = raw_blocks.len();
 
@@ -97,24 +102,37 @@ impl AssetLoader for BlockLibraryLoader {
             let mut variants = Vec::with_capacity(capacity);
 
             for (i, (name, variant)) in raw_blocks.into_iter().enumerate() {
-				let variant = {
-					let raw::BlockVariant { display_name, collision_aabbs, block_model, is_transparent} = variant;
-					let block_model = match block_model {
-						raw::BlockModel::Empty => BlockModel::Empty,
-						raw::BlockModel::Cube(c) => BlockModel::Cube(c),
-						raw::BlockModel::Mesh(m) => {
-							let raw::BlockModelMesh { material_index, path} = m;
+                let variant = {
+                    let raw::BlockVariant {
+                        display_name,
+                        collision_aabbs,
+                        block_model,
+                        is_transparent,
+                    } = variant;
+                    let block_model = match block_model {
+                        raw::BlockModel::Empty => BlockModel::Empty,
+                        raw::BlockModel::Cube(c) => BlockModel::Cube(c),
+                        raw::BlockModel::Mesh(m) => {
+                            let raw::BlockModelMesh {
+                                material_index,
+                                path,
+                            } = m;
 
-							let handle = load_context.load(path);
-							let m = BlockModelMesh {
-								handle,
-								material_index,
-							};
-							BlockModel::Mesh(m)
-						},
-					};
-					BlockVariant { display_name, collision_aabbs, block_model, is_transparent }
-				};
+                            let handle = load_context.load(resolve_path(load_context, &path));
+                            let m = BlockModelMesh {
+                                handle,
+                                material_index,
+                            };
+                            BlockModel::Mesh(m)
+                        }
+                    };
+                    BlockVariant {
+                        display_name,
+                        collision_aabbs,
+                        block_model,
+                        is_transparent,
+                    }
+                };
 
                 name_to_index.insert(name.clone(), i);
                 index_to_name.push(name);
@@ -133,13 +151,23 @@ impl AssetLoader for BlockLibraryLoader {
     }
 }
 
+fn resolve_path(load_context: &LoadContext, path: &str) -> PathBuf {
+    if path.starts_with("/") {
+        PathBuf::from(path.trim_start_matches('/'))
+    } else {
+        let mut path_buf = PathBuf::new();
+        path_buf.push(load_context.path().parent().unwrap_or(Path::new("")));
+        path_buf.push(path);
+        path_buf
+    }
+}
+
 pub struct BlockLibraryPlugin;
 
 impl Plugin for BlockLibraryPlugin {
     fn build(&self, app: &mut App) {
-        app
-			.add_plugins(MaterializePlugin::new(JsonMaterialDeserializer))
-			.init_asset::<BlockLibrary>()
+        app.add_plugins(MaterializePlugin::new(JsonMaterialDeserializer))
+            .init_asset::<BlockLibrary>()
             .init_asset_loader::<BlockLibraryLoader>();
     }
 }
