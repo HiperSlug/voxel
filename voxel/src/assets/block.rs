f2429bfef2521fafa3d4e8a5d447161a6062bcbe
Raphael <raphael.larsen@proton.me>
2025-08-14 10:53:37 -0400

    Changing serialization again.

--- voxel/src/assets/block.rs
+++ voxel/src/assets/block.rs
@@ -1,21 +1,25 @@
+use std::collections::HashMap;
+
 use bevy::{
     asset::{AssetLoader, LoadContext, io::Reader},
     math::bounding::Aabb3d,
     prelude::*,
     tasks::ConditionalSendFuture,
 };
+use math::SignedAxis;
 use serde::{Deserialize, Serialize};
+use serde_json::de::from_slice as json_de;
 
 #[derive(Debug, Serialize, Deserialize, Asset, TypePath)]
-pub struct Block {
+pub struct IntermediateBlock {
     pub display_name: String,
     pub collision_aabbs: Vec<Aabb3d>,
     pub is_translucent: bool,
-    pub textures: Textures,
+    pub textures: IntermediateTextures,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Textures {
+pub struct IntermediateTextures {
     pub pos_x: String,
     pub neg_x: String,
     pub pos_y: String,
@@ -25,10 +29,10 @@ pub struct Textures {
 }
 
 #[derive(Debug, Default)]
-pub struct BlockLoader;
+pub struct IntermediateBlockLoader;
 
-impl AssetLoader for BlockLoader {
-    type Asset = Block;
+impl AssetLoader for IntermediateBlockLoader {
+    type Asset = IntermediateBlock;
     type Settings = ();
     type Error = anyhow::Error;
 
@@ -46,8 +50,78 @@ impl AssetLoader for BlockLoader {
             let mut buffer = Vec::new();
             reader.read_to_end(&mut buffer).await?;
 
-            let out = serde_json::de::from_slice::<Block>(&buffer)?;
-            Ok(out)
+            Ok(json_de(&buffer)?)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Block {
+    pub display_name: String,
+    pub collision_aabbs: Vec<Aabb3d>,
+    pub is_translucent: bool,
+    pub textures: Textures,
+}
+
+impl Block {
+    pub fn from_intermediate(intermediate: IntermediateBlock, texture_context: HashMap<String, usize>) -> Option<Self> {
+        let IntermediateBlock {
+            display_name,
+            collision_aabbs,
+            is_translucent,
+            textures,
+        } = intermediate;
+
+        Some(Self {
+            display_name,
+            collision_aabbs,
+            is_translucent,
+            textures: Textures::from_intermediate(textures, texture_context)?
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Textures {
+    pub pos_x: usize,
+    pub neg_x: usize,
+    pub pos_y: usize,
+    pub neg_y: usize,
+    pub pos_z: usize,
+    pub neg_z: usize,
+}
+
+impl Textures {
+    pub fn from_intermediate(intermediate: IntermediateTextures, texture_context: HashMap<String, usize>) -> Option<Self> {
+        let IntermediateTextures {
+            pos_x,
+            neg_x,
+            pos_y,
+            neg_y,
+            pos_z,
+            neg_z,
+        } = intermediate;
+
+        Some(Self {
+            pos_x: *texture_context.get(&pos_x)?,
+            neg_x: *texture_context.get(&neg_x)?,
+            pos_y: *texture_context.get(&pos_y)?,
+            neg_y: *texture_context.get(&neg_y)?,
+            pos_z: *texture_context.get(&pos_z)?,
+            neg_z: *texture_context.get(&neg_z)?,
+        })
+    }
+
+    pub fn get(&self, signed_axis: SignedAxis) -> usize {
+        use SignedAxis::*;
+
+        match signed_axis {
+            PosX => self.pos_x,
+            NegX => self.neg_x,
+            PosY => self.pos_y,
+            NegY => self.neg_y,
+            PosZ => self.pos_z,
+            NegZ => self.neg_z,
         }
     }
 }
--- voxel/src/assets/mod.rs
+++ voxel/src/assets/mod.rs
@@ -1,4 +1,5 @@
 mod block;
 pub mod block_library;
+pub mod block_library_v2;
 mod load_folders;
 pub mod textures;
